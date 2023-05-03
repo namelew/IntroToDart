d54e8452c5eeb1c108f03c475ab1eda3028c6c41
namelew <diogomaciel.cunha@gmail.com>
2023-05-03 09:50:01 -0300

    usando listas em strings

--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
 void main(List<String> args) {
   List<dynamic> listadiogo = ['Diogo', 'Maciel da Cunha', 26, 1.77];
-  print(listadiogo);
+  print("Oi sou ${listadiogo[0]} ${listadiogo[1]}, tenho ${listadiogo[2]} anos e ${listadiogo[3]} metros de altura");
 }
