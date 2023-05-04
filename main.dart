90d83e1261322c7b07b30bdb96efdd329d5b7799
namelew <diogomaciel.cunha@gmail.com>
2023-05-04 08:07:28 -0300

    aprendendo sobre const.final e var

--- main.dart
+++ main.dart
@@ -1,4 +1,8 @@
 void main(List<String> args) {
-  List<dynamic> listadiogo = ['Diogo', 'Maciel da Cunha', 26, 1.77];
-  print("Oi sou ${listadiogo[0]} ${listadiogo[1]}, tenho ${listadiogo[2]} anos e ${listadiogo[3]} metros de altura");
+  const String nome = 'Diogo Maciel da Cunha'; // const é const
+  var idade = 21; // funciona como o := de Go
+  final double altura = 1.77; // const que permite apenas um assingn
+
+  List<dynamic> listadiogo = [nome, idade, altura];
+  print("Oi sou ${listadiogo[0]}, tenho ${listadiogo[1]} anos e ${listadiogo[2]} metros de altura");
 }
