abe4846efed22bd41bbf27aa8a9596947b8d4940
namelew <diogomaciel.cunha@gmail.com>
2023-05-04 08:23:21 -0300

    aprendendo sobre while em Dart

--- main.dart
+++ main.dart
@@ -2,6 +2,7 @@ void main(List<String> args) {
   const String nome = 'Diogo Maciel da Cunha'; // const é const
   var idade = 21; // funciona como o := de Go
   final double altura = 1.77; // const que permite apenas um assingn
+  int energia = 100;
 
   List<dynamic> listadiogo = [nome, idade, altura];
   print("Oi sou ${listadiogo[0]}, tenho ${listadiogo[1]} anos e ${listadiogo[2]} metros de altura");
@@ -12,11 +13,12 @@ void main(List<String> args) {
     print("$nome é menor de idade");
   }
 
-  listadiogo.forEach((element) {
-    print(element);
-  });
-
   for (int i = 0; i < 5; i++) {
     print("Conclui $i voltas");
+  }
+
+  while (energia > 0) {
+    print("Mais uma repetição");
+    energia -= 6;
   }
 }
